060edf0344c1fe5a579f93fc73c3ea49081cd67a
Maciej Sikora <maciek.sikora84@gmail.com>
2024-01-24 13:30:10 +0100

    Add project files.

---
+++ Zadanie1Warunki/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 5;
+int b = 6;
+
+if(a == b)
+{
+    Console.WriteLine($"Variable a is equal variable b and is: {a}");
+}
+else
+{
+    Console.WriteLine($"Variable a is not equal variable b. Variable a is equal {a} and variable b is equal {b}");
+}
